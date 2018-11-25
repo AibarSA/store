0fa1f1432e7fdd509c2cca29b94c91c403d2b4bc
Aibar-B <aibro_85@mail.ru>
2018-11-25 22:21:49 +0600

    add remove method to controller

--- src/main/java/com/aibar/ecommerce/store/resource/BookResource.java
+++ src/main/java/com/aibar/ecommerce/store/resource/BookResource.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
@@ -95,5 +96,17 @@ public class BookResource {
     @RequestMapping(value = "update", method = RequestMethod.POST)
     public Book updateBookPost(@RequestBody Book book){
         return bookService.save(book);
+    }
+
+
+    @RequestMapping(value = "/remove", method = RequestMethod.POST)
+    public ResponseEntity remove(@RequestBody String id) throws IOException {
+        bookService.removeOne(Long.parseLong(id));
+
+//        String fileName = id + ".png";
+//
+//        Files.delete(Paths.get("/home/andreimerfu/Documents/bookstore-angular/src/bookstore/src/main/resources/static/image/book/" + fileName));
+
+        return new ResponseEntity("Remove Success!", HttpStatus.OK);
     }
 }
